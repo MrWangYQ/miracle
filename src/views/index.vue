85816b0b8c6b7c1bf1157c01f8c18d7cb79cdce1
wangyaqiang <wangyaqiang@51talk.com>
2020-04-02 16:03:25 +0800

    table

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,25 +1,56 @@
 <template>
   <div>
     {{ msg }}
+    <div v-for="c in category" :key="c.name">
+      {{c.name}}
+      <input v-for="item in c.items" v-model="item.value" :key="item.value"/>
+    </div>
+    <div v-for="item in items" :key="item.value">
+      <input v-model="item.value" />
+    </div>
+    <div v-for="item in category | get_items" :key="item.value">
+      <input v-model="item.value" />
+    </div>
+    {{category | json}}
     <!-- <p> 登录注册 怎么验证 验证码 ，后端设置（登录权限）  代码测试 上线 </p> -->
   </div>
 </template>
 
 <script>
 import { postFromData } from '../services/axios';
+// import pinyin from '../utils/Convert_Pinyin.js';
 
 export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Home'
+      msg: 'Home',
+      category: [{name: 'cate0', items: [{ value: 'cate0 item0' }]}, {name: 'cate1', items: [{ value: 'cate1 item1' }]}]
     };
   },
+  filters: {
+    get_items: function (category) {
+      var arr = [];
+      category.forEach(element => element.items.forEach(t => arr.push(t)));
+      return arr;
+    }
+  },
+  computed: {
+    items () {
+      return this.$eval('category | get_items');
+    }
+  },
   mounted () {
-    this.getData();
-    this.getHistory();
+    // this.getData();
+    // this.getHistory();
+    console.log('hhaahahaha');
+    // console.log(pinyin.getFullChars('哈哈哈'), "pinyin('哈哈哈')");
   },
   methods: {
+    eval (fn) {
+      let Fn = Function;
+      return new Fn('return ' + fn)();
+    },
     getData () {
       postFromData('/device/enable')
         .then(({code, data, msg}) => {
